90cd8c3f2135b3ec9099887f4f6bd882af8e294f
Jens-Ole Graulund <jensole@graulund.net>
2014-08-18 18:30:18 +0200

    error in package.json

--- test/index.js
+++
@@ -1,9 +0,0 @@
-var expect = require('expect.js'),
-    devices = require('..');
-
-describe('devices', function() {
-  it('should say hello', function(done) {
-    expect(devices()).to.equal('Hello, world');
-    done();
-  });
-});
